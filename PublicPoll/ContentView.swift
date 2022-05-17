c154e45bb3d5bbf1155e2d4a5da1c2f546f58d25
hanuuuuU <han980004@naver.com>
2022-05-17 12:39:29 +0900

    Initial Commit

---
+++ PublicPoll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PublicPoll
+//
+//  Created by 정우 on 2022/05/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PublicPoll/PublicPollApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicPollApp.swift
+//  PublicPoll
+//
+//  Created by 정우 on 2022/05/17.
+//
+
+import SwiftUI
+
+@main
+struct PublicPollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
